e88896ae262404b6218a21ced0818f36b0d738f3
Tom <t.bartleet@hotmail.co.uk>
2018-12-17 09:21:49 +0000

    First Commit
    First Commit

---
+++ SimpleXam.TintedImagePlugin/SimpleXam.TintedImagePlugin.iOS/TintedImageRenderer.cs
@@ -0,0 +1,53 @@
+﻿using SimpleXam.TintedImagePlugin;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRendererAttribute(typeof(TintedImage), typeof(TintedImageRenderer))]
+namespace SimpleXam.TintedImagePlugin
+{
+    public class TintedImageRenderer : ImageRenderer
+    {
+        private TintedImage CustomElement => Element as TintedImage;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            SetTint();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if ((e.PropertyName == TintedImage.TintColorProperty.PropertyName) ||
+                (e.PropertyName == TintedImage.SourceProperty.PropertyName))
+            {
+                SetTint();
+            }
+        }
+
+        void SetTint()
+        {
+            if ((Control == null) ||
+                (Control.Image == null) ||
+                (Element == null))
+            {
+                return;
+            }
+
+            if (CustomElement.TintColor == Color.Transparent)
+            {
+                //Turn off tinting
+                Control.Image = Control.Image.ImageWithRenderingMode(UIKit.UIImageRenderingMode.Automatic);
+                Control.TintColor = null;
+            }
+            else
+            {
+                //Apply tint color
+                Control.Image = Control.Image.ImageWithRenderingMode(UIKit.UIImageRenderingMode.AlwaysTemplate);
+                Control.TintColor = CustomElement.TintColor.ToUIColor();
+            }
+        }
+    }
+}
